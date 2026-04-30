60256215e2564ab32ed2d03d0697853393bc2e9e
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-30 17:31:53 +0800

    Added the floor division logic

--- calculator_operations.py
+++ calculator_operations.py
@@ -18,3 +18,8 @@ class CalculatorOperation:
         if second_number == 0:
             raise ZeroDivisionError("Cannot divide by zero!!!")
         return first_number % second_number
+
+    def floor_division(self, first_number, second_number):
+        if second_number == 0:
+            raise ZeroDivisionError("Cannot divide by zero!!!")
+        return first_number // second_number
