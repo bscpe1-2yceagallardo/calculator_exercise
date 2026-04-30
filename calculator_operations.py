ce4be75ba6b597cc8adc55d4dc4b67d459ef3c21
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-30 17:30:23 +0800

    Added the modulus logic

--- calculator_operations.py
+++ calculator_operations.py
@@ -13,3 +13,8 @@ class CalculatorOperation:
         if second_number == 0:
             raise ZeroDivisionError ("Cannot divide by zero!!!")
         return first_number / second_number
+
+    def modulus(self, first_number, second_number):
+        if second_number == 0:
+            raise ZeroDivisionError("Cannot divide by zero!!!")
+        return first_number % second_number
