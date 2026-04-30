2a41031ebac4137f68b4b22344b5afe25e651ba1
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-04-30 17:27:40 +0800

    Added the division logic

--- calculator_operations.py
+++ calculator_operations.py
@@ -8,3 +8,8 @@ class CalculatorOperation:
 
     def multiplication(self, first_number, second_number):
         return first_number * second_number
+
+    def division(self, first_number, second_number):
+        if second_number == 0:
+            raise ZeroDivisionError ("Cannot divide by zero!!!")
+        return first_number / second_number
